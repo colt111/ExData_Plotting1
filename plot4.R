8dbf739b51134e9dc05eb27300b2ad80532e320d
colt111 <cfloyd@gmail.com>
2014-06-08 19:22:48 -0400

    added answer for problem 4

--- plot4.R
+++ plot4.R
@@ -21,3 +21,14 @@ getDataFrame <- function(f) {
 
 datafile <-'household_power_consumption.txt'
 testdf<-getDataFrame(datafile)
+par(mfrow = c(2,2))
+plot(testdf$Time, testdf$Global_active_power, ylab = 'Global Active Power', xlab= '', type='l')
+plot(testdf$Time, testdf$Voltage, ylab = 'Voltage', type = 'l', xlab ='')
+plot(testdf$Time,testdf$Sub_metering_1, , xlab = '', ylab = 'Energy sub metering', type = 'n')
+points(testdf$Time,testdf$Sub_metering_1, col = 'black', type = 'l')
+points(testdf$Time,testdf$Sub_metering_2, col = 'red', type = 'l')
+points(testdf$Time,testdf$Sub_metering_3, col = 'blue', type = 'l')
+legend('topright', legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), pch='-', col = c('black', 'red', 'blue'))
+plot(testdf$Time, testdf$Global_reactive_power, ylab = 'Global_reactive_power', xlab = 'datetime', type = 'l')
+dev.copy(png, file = 'plot4.png')
+dev.off()
